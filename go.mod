82c0f24742c0060a09cef3b76e0390fbc441f5cf
Schmid Lothar <web@jagolo.ch>
2022-05-06 07:58:51 +0000

    New Data

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/schlucht/bills
+
+go 1.14
--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+
+  "github.com/schlucht/bills/model"
+  )
 
 func main() {
-  fmt.Println("Hello World")
+  b := Bill {
+    Text: "Hallo",
+    Amount: 12.3,
+    Ballance: 1235.5896,
+  }
+  fmt.Println(b.ToString())
   fmt.Println("Hier ist die zweite Meldung")
 }

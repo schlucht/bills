e822127df93b4bb1de768ee201f421a0fbf62f00
Schmid Lothar <web@jagolo.ch>
2022-07-02 07:12:09 +0200

    Erste eingabe

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 
-  "github.com/schlucht/bills/model"
-  )
+	bill "github.com/schlucht/bills/model"
+)
 
 func main() {
-  b := Bill {
-    Text: "Hallo",
-    Amount: 12.3,
-    Ballance: 1235.5896,
-  }
-  fmt.Println(b.ToString())
-  fmt.Println("Hier ist die zweite Meldung")
+	b := bill.Bill{
+		Text:     "Hallo",
+		Amount:   12.3,
+		Ballance: 1235.5896,
+	}
+	fmt.Println(b.ToString())
+	fmt.Println("Hier ist die zweite Meldung")
 }
